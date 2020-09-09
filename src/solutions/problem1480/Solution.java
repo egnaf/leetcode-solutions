2e50c61660539179de45f55f52554b6335e6427f
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-09-09 19:01:01 +0300

    Add 1480 problem solution

---
+++ src/solutions/problem1480/Solution.java
@@ -0,0 +1,14 @@
+package solutions.problem1480;
+
+class Solution {
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
+}
